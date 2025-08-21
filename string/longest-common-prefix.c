6e2876f1575c325bf44c5bcd01e54df3589795cd
FFunWay <93251880+FFunWay@users.noreply.github.com>
2025-08-21 22:00:34 +0800

    LC-longest-common-prefix [Runtime: 5 ms; Memory: 8.4 MB]

---
+++ string/longest-common-prefix.c
@@ -0,0 +1,15 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0){
+        return "";
+    }
+
+    static char prefix[201];
+    strcpy(prefix, strs[0]);
+    for(int i = 1; i <strsSize; i++) {
+        while(strncmp(prefix, strs[i], strlen(prefix))!= 0){
+            prefix[strlen(prefix)-1] = '\0';
+            
+        }
+    }
+    return prefix;
+}
