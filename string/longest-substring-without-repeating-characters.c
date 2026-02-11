f29ec690a9db94376d01634917bbf6b7057532b8
FFunWay <93251880+FFunWay@users.noreply.github.com>
2026-02-11 16:40:42 +0800

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- string/longest-substring-without-repeating-characters.c
+++ string/longest-substring-without-repeating-characters.c
@@ -1,20 +1,19 @@
 int lengthOfLongestSubstring(char* s) {
+    int last[256];
+    for (int i = 0; i < 256 ; i++){
+        last[i] = -1;
+    }
+    int start = 0;
     int best = 0;
 
-    for (int i = 0; s[i] != '\0'; i++) {
-        int seen[256] = {0};
-        int j = i;
-
-        while (s[j] != '\0') {
-            int idx = (unsigned char)s[j];
-            if (seen[idx]) break;
-            seen[idx] = 1;
-            j++;
+    for (int i =0; s[i] != '\0'; i++) {
+        unsigned char c = unsigned char s[i];
+        if (len[c] >= start) {
+            start = last[c] + 1;
         }
-        int len = j -i;
-        if (len > best){
-            best = len;
-        }
+        last[c] = i;
+        int len = i -start +1;
+        if (len > best) best = len; 
     }
     return best;
 }
