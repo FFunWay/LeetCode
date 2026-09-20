3a5f3adf9a284e33fa5758c6679842bb3626e877
FFunWay <93251880+FFunWay@users.noreply.github.com>
2026-09-20 14:25:01 +0800

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

---
+++ bucket-sort/top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) +1
+        
+        bucket = [[] for _ in range(len(nums)+1)]
+        for num,freq in count.items():
+            bucket[freq].append(num)
+        
+        result = []
+        for i in range(len(bucket)-1, 0, -1):
+            for num in bucket[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
